abaef828f542eadf1b17bf01f22c1b9a034b7285
gaffOverflow <gaffar.ibrahim18@gmail.com>
2023-07-21 08:32:24 -0700

    home-Section and skills-Section completed for mobile orientation

--- js/main.js
+++ js/main.js
@@ -1,29 +1,37 @@
+const navMenu = document.querySelector("#nav-menu");
+const navToggle = document.querySelector("#nav-toggle");
+const navClose = document.querySelector("#nav-close");
+const navLink = document.querySelectorAll(".nav__item");
+
 /*=============== SHOW MENU ===============*/
+navToggle.addEventListener("click", () => {
+  navMenu.classList.add("nav__show");
+});
+navClose.addEventListener("click", () => {
+  navMenu.classList.toggle("nav__show");
+});
+
+navLink.forEach( link => {
+  link.addEventListener("click", () => {
+  navMenu.classList.toggle("nav__show");
+  })
+})
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== SWIPER PROJECTS ===============*/
-
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
 /*=============== EMAIL JS ===============*/
-
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
