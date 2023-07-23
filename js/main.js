50f77659ee6e65d0ca5054e0c95d3e8bccf4c96e
gaffOverflow <gaffar.ibrahim18@gmail.com>
2023-07-23 04:50:17 -0700

    project section added

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const navToggle = document.querySelector("#nav-toggle");
 const navClose = document.querySelector("#nav-close");
 const navLink = document.querySelectorAll(".nav__item");
 
-/*=============== SHOW MENU ===============*/
+/*=============== TOGGLE MENU ===============*/
+
 navToggle.addEventListener("click", () => {
   navMenu.classList.add("nav__show");
 });
@@ -11,16 +12,34 @@ navClose.addEventListener("click", () => {
   navMenu.classList.toggle("nav__show");
 });
 
-navLink.forEach( link => {
+navLink.forEach((link) => {
   link.addEventListener("click", () => {
-  navMenu.classList.toggle("nav__show");
-  })
-})
-
-
-/*=============== REMOVE MENU MOBILE ===============*/
+    navMenu.classList.toggle("nav__show");
+  });
+});
 
 /*=============== SWIPER PROJECTS ===============*/
+
+let swiperProjects = new Swiper(".projects__container", {
+  loop: true,
+  spaceBetween: 24,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // mousewheel: true,
+  // keyboard: true,
+  breakpoints: {
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: -56,
+    },
+  },
+});
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
